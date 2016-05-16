4b49be670803beb0c7d122a226d33857f5822d5e
JoakimLangelaar <joakimlangelaar@hotmail.com>
2016-05-16 15:21:46 +0200

    Update StudentStartUp.js

--- StudentStartUp.js
+++ StudentStartUp.js
@@ -1,1 +1,28 @@
+function validate ()
+{
+  var inputnamn = document.forms["registerForm"]["namn"].value;
+  var inputmail = document.forms["registerForm"]["mail"].value;
+  var inputlösen = document.forms["registerForm"]["lösenord"].value;
+  var lösencheck = document.forms["registerForm"]["valLösen"].value;
+
+  if (inputlösen == "" || inputlösen == null)
+  {
+    alert("Skriv ett lösenord!");
+    return false;
+  }
+  if (inputmail =="" || inputmail == null)
+  {
+    alert("Mail är inkorrekt");
+    return false;
+  }
+  if (inputnamn == null || inputnamn == "")
+  {
+    alert("Du måste fylla i ett namn!");
+    return false;
+  }
+  if (inputlösen != lösencheck)
+  {
+    alert("Lösenorden matchar inte"); 
+  }
+}
 
